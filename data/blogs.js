1a17b49d266221516e6cbfd1cb8379e6a9482d8f
Amit Dubey <amitdubey6261@gmail.com>
2022-03-26 13:21:51 +0530

    basic file structure

---
+++ data/blogs.js
@@ -0,0 +1,25 @@
+//array of objects
+blogs = [
+        { 
+            title: 'How to get started with python',
+            content : 'This is python content',
+            slug : 'python-learn',
+        } , 
+        { 
+            title: 'How to get started with js',
+            content : 'This is js content',
+            slug : 'js-learn',
+        } , 
+        { 
+            title: 'How to get started with cpp',
+            content : 'This is cpp content',
+            slug : 'cpp-learn',
+        } , 
+        { 
+            title: 'How to get started with java',
+            content : 'This is java content',
+            slug : 'java-learn',
+        } 
+    ]
+
+module.exports = blogs ; 
---
+++ routes/blog.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const blogs = require('../data/blogs');
+
+const router = express.Router();
+
+router.get('/' , (req , res) => {
+    res.sendFile(path.join(__dirname, '../templates/index.html'));
+})
+
+router.get('/blog' , (req , res) => {
+    // blogs.forEach(e =>{
+    //     console.log(e.title);
+    // })                                                           this code prints data in console ...! 
+    res.sendFile(path.join(__dirname, '../templates/blogHome.html'));
+})
+
+router.get('/blogpost/:slug' , (req , res) => {
+    // myBlog = blogs.filter( (e) => {
+    //     return e.slug === req.params.slug;
+    // })
+    myBlog = blogs.filter(e =>{
+        return e.slug == req.params.slug ;
+    })
+    console.log(myBlog);
+    res.sendFile(path.join(__dirname, '../templates/blogPage.html'));
+})
+
+router.get('/' , (req , res) => {
+    res.sendFile(path.join(__dirname, '../templates/index.html'));
+})
+
+
+module.exports = router;
